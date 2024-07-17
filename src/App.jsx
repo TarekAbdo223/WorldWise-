a13c84f74cb0c862d1ffb0f9cf142e6aa8aec73d
Tarek Abdo <tarekk.abdo22@gmail.com>
2024-07-17 20:53:11 +0300

    start the real work using the fake api

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,32 @@ import NotFound from "./Pages/NotFound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "./Pages/AppLayout";
 import Login from "./Pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
 
 const App = () => {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCity() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        console.log(data);
+        setCities(data);
+      } catch (error) {
+        alert("there was an error fetching cities");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCity();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,7 +40,14 @@ const App = () => {
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/app" element={<AppLayout />}>
-          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
